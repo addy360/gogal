9cec6a0c639fa6189cdd3a23a070f14761b629d0
addy360 <abbyboy360@gmail.com>
2021-04-09 13:52:16 +0300

    added gorilla mux

--- main.go
+++ main.go
@@ -4,26 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handleFunc(rw http.ResponseWriter, r *http.Request) {
+func home(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "text/html")
-
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(rw, "Welcome to my gallery")
-	case "/about":
-		fmt.Fprint(rw, "About page")
-	default:
-		rw.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(rw, "Page not found")
-	}
+	fmt.Fprint(rw, "Welcome to my gallery")
 
 }
 
+func about(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(rw, "About my gallery")
+}
+
 func main() {
-	http.HandleFunc("/", handleFunc)
-	err := http.ListenAndServe(":8989", nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/about", about)
+	err := http.ListenAndServe(":8989", r)
 	if err != nil {
 		log.Panic(err)
 	}
