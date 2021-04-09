0aa6cc05d8925373e5bdcf5cd333c610c5bc5a94
addy360 <abbyboy360@gmail.com>
2021-04-09 14:12:52 +0300

    added 404 handler

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type _404 struct {
+}
+
+func (notFound *_404) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	rw.WriteHeader(http.StatusNotFound)
+
+	fmt.Fprint(rw, "Sorry, the page you are requesting does not exist!")
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(rw, "Welcome to my gallery")
@@ -24,6 +34,9 @@ func main() {
 
 	r.HandleFunc("/", home)
 	r.HandleFunc("/about", about)
+
+	r.NotFoundHandler = &_404{}
+
 	err := http.ListenAndServe(":8989", r)
 	if err != nil {
 		log.Panic(err)
